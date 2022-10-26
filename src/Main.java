2cc30b37e3ab29d60e6b7ed2f4f438da949830ed
blazej <wituckib@gmail.com>
2022-10-26 19:58:36 +0200

    obiekty kroliki

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         Rabbit krolik1=new Rabbit(100,"Jędruś", true);
-        Rabbit krolik2=new Rabbit(100,"Hanek", false);
-
-
+        krolik1.changeName("Ambroży");
+        System.out.println(krolik1);
 
 
     }
--- src/Rabbit.java
+++ src/Rabbit.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Rabbit {
-    int length;
-    String name;
-    boolean softness;
-    Rabbit(int length, String name, boolean softness){
+    private int length;
+    private String name;
+    private boolean softness;
+   Rabbit(int length, String name, boolean softness){
         this.length=length;
         this.name=name;
         this.softness=softness;
-    }
+   }
+        //metoda dla ustawienia nowej nazwy krolika
+   public void changeName (String name){
+       this.name=name;
+
+
+   }
+
 }
