c4e9e421352618e5f2cbd94a0570d230022714af
blazej <wituckib@gmail.com>
2022-10-26 19:29:59 +0200

    obiekty

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Rabbit krolik1=new Rabbit(100,"Jędruś", true);
+        Rabbit krolik2=new Rabbit(100,"Hanek", false);
+
+
+
+
     }
 }
---
+++ src/Rabbit.java
@@ -0,0 +1,10 @@
+public class Rabbit {
+    int length;
+    String name;
+    boolean softness;
+    Rabbit(int length, String name, boolean softness){
+        this.length=length;
+        this.name=name;
+        this.softness=softness;
+    }
+}
